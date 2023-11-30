3da6fe35cfe3e9cf8ad71bd2ab9b628f6a2b0843
juve2000 <stanislav.litvinov@gbmf.tech>
2023-11-30 17:10:58 +0200

    d

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import * as io from "socket.io-client";
 
+// export const socket = io.connect(
+//   "https://87.248.167.60:4000/?channel=eld:realtime:gps:229LA2023000002"
+// );
+
 export const socket = io.connect(
-  "http://87.248.167.60:4000/?channel=eld:realtime:gps:229LA2023000002"
+  "https://websocket.quirindevelopment.tech/?channel=eld:realtime:gps:229LA2023000002"
 );
 
 // export const useSocket = (props: any) => {]
